687f22b4af80446b881222c28810a58e4e9e3bde
taetaepao <54477787+taetaepao@users.noreply.github.com>
2025-04-06 20:15:55 +0700

    start

---
+++ src/app.test.js
@@ -0,0 +1,12 @@
+const { dayOfTheWeek, monthOfTheYear } = require('./app');
+
+
+test('getDay returns the long-format day of the week', () => {
+    const day = dayOfTheWeek( new Date('3/11/2020') );
+    expect( day ).toBe('Wednesday');
+});
+
+test('getMonth returns the long-format month of the year', () => {
+    const month = monthOfTheYear(new Date('3/11/2020'));
+    expect(month).toBe('November');
+});
